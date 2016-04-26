eb02d2e7a383258bbe96b72f90a1e349b7e2a5e8
wangjiezhe <wangjiezhe@gmail.com>
2016-04-26 11:10:40 +0800

    Fix key map for avy

--- rc/rc-avy.el
+++ rc/rc-avy.el
@@ -7,13 +7,13 @@
 ;; (global-set-key (kbd "C-'") 'avy-goto-char-2)
 
 ;;; input zero char, jump to a line start with a tree
-;; (global-set-key (kbd "M-g f") 'avy-goto-line)
+(global-set-key (kbd "M-g f") 'avy-goto-line)
 
 ;;; input one char at word start, jump to a word start with a tree
-;; (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
 
 ;;; input zero char, jump to a word start with a tree
-;; (global-set-key (kbd "M-g e") 'avy-goto-word-0)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
 
 (global-set-key (kbd "C-'") 'avy-goto-word-or-subword-1)
 
