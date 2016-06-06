29b0f1e61ad63e1e2a9792fba26bbd3f4588a8c3
wangjiezhe <wangjiezhe@gmail.com>
2016-06-06 19:40:26 +0800

    Travis CI: fix

--- .travis-build.sh
+++ .travis-build.sh
@@ -3,9 +3,8 @@
 echo "Attempting startup..."
 
 ${EMACS:=emacs} --batch --eval \
-                '(let ((debug-on-error t)
-                       (user-init-file (expand-file-name "init.el")))
+                '(let ((user-init-file (expand-file-name "init.el")))
                    (load-file user-init-file)
-                   (run-hook (quote after-init-hook)))'
+                   (run-hooks (quote after-init-hook)))'
 
 echo "Startup successful"
