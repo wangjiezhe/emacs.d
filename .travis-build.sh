4515c5a4093ee2e58fa6e319b54493c7f08b39d5
wangjiezhe <wangjiezhe@gmail.com>
2016-06-07 13:36:34 +0800

    Travis: update

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
-echo -e "Attempting startup...\n"
+echo "Attempting startup...\n"
 
 ${EMACS:=emacs} -nw -batch -eval \
                 '(let ((user-init-file (expand-file-name "init.el")))
                    (load-file user-init-file)
                    (run-hooks (quote after-init-hook)))'
 
-echo -e "\nStartup successful"
+echo "\nStartup successful"
