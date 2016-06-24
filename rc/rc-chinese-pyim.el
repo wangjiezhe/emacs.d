096de8ef0a6b3a0b24687efe17cdaa6210aa30bf
wangjiezhe <wangjiezhe@gmail.com>
2016-06-24 10:13:56 +0800

    Update chinese-pyim configuration

--- rc/rc-chinese-pyim.el
+++ rc/rc-chinese-pyim.el
@@ -1,34 +1,31 @@
 ;;; Configuration for chinese-pyim input method
 
-(require 'chinese-pyim )
+(require 'chinese-pyim)
 (setq default-input-method "chinese-pyim")
 
-;;; user dicts
-(set 'pyim-dicts
-   (quote
-    ((:name "BigDict" :file "~/.emacs.d/pyim/dicts/pyim-bigdict.pyim" :coding utf-8-unix :dict-type pinyin-dict))))
+(chinese-pyim-greatdict-enable)
+
+;;; enable pinyin search
+(setq pyim-isearch-enable-pinyin-search t)
 
 ;;; dynamic change between Chinese and English
-(setq-default pyim-english-input-switch-function
-	      'pyim-dynamic-english-input-function)
-
-;; (setq-default pyim-english-input-switch-functions
-;;               '(pyim-probe-dynamic-english
-;;                 pyim-probe-isearch-mode
-;;                 pyim-probe-program-mode
-;;                 pyim-probe-org-structure-template))
+(setq-default pyim-english-input-switch-functions
+              '(pyim-probe-program-mode
+                pyim-probe-isearch-mode
+                pyim-probe-dynamic-english
+                pyim-probe-org-speed-commands
+                pyim-probe-org-structure-template))
 
 (setq-default pyim-punctuation-half-width-functions
               '(pyim-probe-punctuation-line-beginning
                 pyim-probe-punctuation-after-punctuation))
 
-;;; enable pinyin search
-(setq pyim-isearch-enable-pinyin-search t)
-
 (setq pyim-use-tooltip 'popup
       pyim-page-length 5)
 
 ;;; use pyim to improve auto completion for chinese
+(require 'chinese-pyim-company)
 (setq pyim-company-max-length 6)
+
 
 (provide 'rc-chinese-pyim)
