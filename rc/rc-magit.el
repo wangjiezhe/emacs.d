b912be19649d0a066d9ae2bae887909721b14494
wangjiezhe <wangjiezhe@gmail.com>
2017-04-28 19:06:58 +0800

    Fix: open too many emacs windows when starting from shell

--- rc/rc-magit.el
+++ rc/rc-magit.el
@@ -1,14 +1,15 @@
 ;; Configuration for magit-mode
 
-(require 'magit-mode)
-(require 'magit-process)
+(autoload 'magit-after-save-refresh-status "magit-mode" "Refresh the status buffer of the current repository." t)
+(autoload 'magit-process-file "magit-process" "Process files synchronously in a separate process." t)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
 (add-hook 'after-save-hook 'magit-after-save-refresh-status)
 
 ;;; use ivy
 (setq magit-completing-read-function 'ivy-completing-read)
 
-;;; git wip
-;; (magit-wip-after-save-mode)
 
 (provide 'rc-magit)
